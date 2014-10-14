f1af0291ed2312303fc8bed51ab370cd784c048a
Muhammed Thanish <mnmtanish@gmail.com>
2014-10-14 11:54:57 +0530

    Add basic dispatcher

---
+++ index.js
@@ -0,0 +1,42 @@
+var _ = require('underscore');
+var util = require('util');
+var events = require('events');
+var Client = require('./lib/client.js');
+
+function MeteorShower(action) {
+  this.action = action;
+}
+
+MeteorShower.prototype.run = function(options) {
+  for(var i=0; i<options.concurrency; ++i) {
+    this.dispatch(options);
+  }
+};
+
+MeteorShower.prototype.dispatch = function(options) {
+  var self = this;
+  var client = new Client({
+    url: options.url,
+    key: options.key,
+    auth: {userId: pickRandom(options.auth.userId)}
+  });
+
+  client.on('disconnected', function () {
+    self.dispatch(options);
+  });
+
+  client.on('socket-error', function(error) {
+    self.dispatch(options);
+  });
+
+  client.init(function (error) {
+    self.action(error, client);
+  });
+};
+
+function pickRandom (array) {
+  return array[_.random(array.length-1)];
+}
+
+module.exports = MeteorShower;
+MeteorShower.Client = Client;
