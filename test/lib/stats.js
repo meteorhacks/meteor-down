f186b3c313f71c38e9ace3063e8f471d6687d0e3
Muhammed Thanish <mnmtanish@gmail.com>
2014-11-03 11:48:14 +0530

    Generalize stat tracking

--- test/lib/stats.js
+++ test/lib/stats.js
@@ -7,65 +7,39 @@ suite('Stats', function () {
     test('reset', function () {
       var mock = {};
       Stats.prototype.reset.call(mock);
-      assert.deepEqual(mock, {
-        data: {
-          'method-response-time': {},
-          'subscription-response-time': {}
-        }
-      });
+      assert.deepEqual(mock, {data: {}});
     });
   })
 
   suite('tracking', function () {
-    test('method-response-time', function () {
-      var mock = {data: {'method-response-time': {}}};
+    test('field-name', function () {
+      var mock = {data: {'field-name': {}}};
       var r = _.random(1000);
-      Stats.prototype.track.call(mock, 'method-response-time', 'x', r);
-      assert.deepEqual(mock.data['method-response-time'], {'x': [r]});
-    });
-
-    test('subscription-response-time', function () {
-      var mock = {data: {'subscription-response-time': {}}};
-      var r = _.random(1000);
-      Stats.prototype.track.call(mock, 'subscription-response-time', 'x', r);
-      assert.deepEqual(mock.data['subscription-response-time'], {'x': [r]});
+      Stats.prototype.track.call(mock, 'field-name', 'x', r);
+      assert.deepEqual(mock.data['field-name'], {'x': [r]});
     });
   });
 
   suite('reporting', function () {
     test('without data', function () {
-      var mock = {data: {
-        'method-response-time': {},
-        'subscription-response-time': {}
-      }};
+      var mock = {data: {'field-name': {}}};
       var stats = Stats.prototype.get.call(mock);
       assert.deepEqual(stats, [
-        { type: 'method-response-time',
-          summary: {total: 0, count: 0, average: 0},
-          breakdown: []},
-        { type: 'subscription-response-time',
+        { type: 'field-name',
           summary: {total: 0, count: 0, average: 0},
           breakdown: []}
       ]);
     });
 
     test('with data', function () {
-      var mock = {data: {
-        'method-response-time': {x: [2, 2, 11], y: [4, 6]},
-        'subscription-response-time': {a: [10]}
-      }};
+      var mock = {data: {'field-name': {x: [2, 2, 11], y: [4, 6]}}};
       var stats = Stats.prototype.get.call(mock);
       assert.deepEqual(stats, [
-        { type: 'method-response-time',
+        { type: 'field-name',
           summary: {total: 25, count: 5, average: 5},
           breakdown: [
             {name: 'x', total: 15, count: 3, average: 5},
             {name: 'y', total: 10, count: 2, average: 5}
-          ]},
-        { type: 'subscription-response-time',
-          summary: {total: 10, count: 1, average: 10},
-          breakdown: [
-            {name: 'a', total: 10, count: 1, average: 10}
           ]}
       ]);
     });
